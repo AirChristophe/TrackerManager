8d4e49b39ab629ebe0c9c4373cd4a70a7577b3a4
Christophe Duval <duval@trimaran.com>
2024-01-10 15:19:51 +0100

    Modifs pour faire marcher la navigation

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,10 @@
-import { Slot } from 'expo-router';
+import { Slot } from "expo-router";
+import { View } from "react-native";
 
 export default function HomeLayout() {
   return (
-    <>
-      <Header />
+    <View style={{ margin: 50 }}>
       <Slot />
-      <Footer />
-    </>
+    </View>
   );
 }
