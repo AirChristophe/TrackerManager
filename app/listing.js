fafd5b01e3a71e253a591b2e043aeea68ac0a27e
cyrille <cyrille.duval@gmail.com>
2024-01-10 15:37:07 +0100

    dev

---
+++ app/listing.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+import { View, FlatList, Text } from "react-native";
+import { Link } from "expo-router";
+import { useFocusEffect } from "expo-router";
+
+export default function Page() {
+  const [datas, setDatas] = useState([]);
+
+  const _fetchData = async () => {
+    const url = "http://0vh-player.georacing.com/datas/events.json";
+    const response = await fetch(url);
+    const d = await response.json();
+
+    console.log(d);
+    setDatas(d);
+  };
+
+  useFocusEffect(() => {
+    _fetchData();
+  });
+
+  return (
+    <View>
+      <Text>LISTING</Text>
+
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={datas}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              //height: 50,
+              borderBottomColor: "#CCCCCC",
+              borderBottomWidth: 1,
+              marginBottom: 5,
+              padding: 5,
+            }}
+          >
+            <Text>{item.name}</Text>
+            <Text>{item.timezone}</Text>
+            <Text>{item.sport}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
