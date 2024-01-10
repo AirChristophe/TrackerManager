a7540fa66e6166c4bd5dcdfb73ea61c68a108362
cyrille <cyrille.duval@gmail.com>
2024-01-10 18:34:37 +0100

    dev

--- app/index.js
+++ app/index.js
@@ -1,30 +1,26 @@
-import { View, Pressable,StyleSheet, Text } from "react-native";
+import { View, Pressable, StyleSheet, Text } from "react-native";
 import { Link } from "expo-router";
-
 
 export default function Page() {
   return (
-    <View  style={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.item}>Home page</Text>
       <Link href="/events">Events</Link>
-      <Link href="/page1">Page 1</Link>    
-
+      <Link href="/page1">Page 1</Link>
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
     margin: 10,
   },
   item: {
-
-    backgroundColor: 'pink',
+    backgroundColor: "pink",
   },
 });
