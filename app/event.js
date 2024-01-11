e6233de469b2c59402e80d25dcb4cb29ead88df6
cyrille <cyrille.duval@gmail.com>
2024-01-11 09:38:34 +0100

    code

--- app/event.js
+++ app/event.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { View,StyleSheet, FlatList, Text, Pressable } from "react-native";
-import { Link, useFocusEffect,useLocalSearchParams } from "expo-router";
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import { widthPixel,heightPixel, fontPixel,pixelSizeVertical,pixelSizeHorizontal} from './fontsize';
+import { View, StyleSheet, FlatList, Text, Pressable } from "react-native";
+import { Link, useFocusEffect, useLocalSearchParams } from "expo-router";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import {
+  widthPixel,
+  heightPixel,
+  fontPixel,
+  pixelSizeVertical,
+  pixelSizeHorizontal,
+} from "./fontsize";
 
 export default function Page() {
   const params = useLocalSearchParams();
 
-console.log(params);
+  console.log(params);
 
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
@@ -15,14 +21,14 @@ console.log(params);
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     };
 
     getBarCodeScannerPermissions();
   }, []);
 
   const handleBarCodeScanned = ({ type, data }) => {
-  console.log("handleBarCodeScanned");    
+    console.log("handleBarCodeScanned");
     setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
@@ -33,49 +39,37 @@ console.log(params);
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-  
-
 
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>{params?.name}</Text>
-        <BarCodeScanner
-          
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        />
-        {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-        
+      <Text style={styles.title}>{params?.name}</Text>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={{ height: 400 }}
+      />
+      {scanned && (
+        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      )}
     </View>
   );
 }
 
-
-
 const styles = StyleSheet.create({
   title: {
-    color:"#014786",
+    color: "#014786",
     fontSize: fontPixel(25),
-    marginTop: 20,
-    marginBottom: 20,
+    marginVertical: 30,
   },
 
   container: {
     display: "flex",
-    //backgroundColor: "#ffff44",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 0,
-    margin: 0,
-    width:"100%"
-  },
-  item: {
-    padding: 10,
-    margin: 5,
-    backgroundColor: "#014786",
-    color:"#FF0000",
-    fontSize: fontPixel(15),
-    alignItems: 'center',
-    justifyContent: "center",
-    width:"100%"
+    flexDirection: "column",
+    backgroundColor: "#FFFFFF",
+    height: "100%",
+    //alignItems: "center",
+    //justifyContent: "center",
+    //padding: 0,
+    //margin: 0,
+    //width: "100%",
   },
 });
