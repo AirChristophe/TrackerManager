1e62b6ff1e39bc3201af758aaa1b25ea90542c01
Christophe Duval <duval@trimaran.com>
2024-01-11 20:32:33 +0100

    modifs

--- app/_layout.js
+++ app/_layout.js
@@ -5,6 +5,14 @@ import { widthPixel,heightPixel, fontPixel,pixelSizeVertical,pixelSizeHorizontal
 
 console.log("layout");
 
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'https://eee40f78fc3eb4bb091cffc7fe96629f@o338749.ingest.sentry.io/4506554246889472',
+  enableInExpoDevelopment: true,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
+
 export default function HomeLayout() {
   return (
     /*
