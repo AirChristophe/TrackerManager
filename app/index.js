49e6cae1ec9b7ec3f5e0db4a7c330a5ac6d64bb4
cyrille <cyrille.duval@gmail.com>
2024-01-12 12:09:57 +0100

    dev

--- app/index.js
+++ app/index.js
@@ -1,25 +1,40 @@
-import { View, Pressable, StyleSheet, Text,Dimensions,PixelRatio  } from "react-native";
+import {
+  View,
+  Pressable,
+  StyleSheet,
+  Text,
+  Dimensions,
+  PixelRatio,
+} from "react-native";
 import { Link } from "expo-router";
-import { widthPixel,heightPixel, fontPixel,pixelSizeVertical,pixelSizeHorizontal} from './fontsize';
-
+import {
+  widthPixel,
+  heightPixel,
+  fontPixel,
+  pixelSizeVertical,
+  pixelSizeHorizontal,
+} from "./fontsize";
+import config from "../config";
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tracker Manager</Text>
-      <View style={styles.menu}>        
-        <Link style={styles.item} href="/events">Events</Link>
-        <Link style={styles.item} href="/tracker_scanner">Scan tracker</Link>
-        
+      <View style={styles.menu}>
+        <Link style={styles.item} href="/events">
+          Events
+        </Link>
+        <Link style={styles.item} href="/tracker_scanner">
+          Scan tracker
+        </Link>
       </View>
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   title: {
-    color:"#014786",
+    color: config.COLOR1,
     fontSize: fontPixel(35),
     marginTop: 20,
     marginBottom: 20,
@@ -32,25 +47,24 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     padding: 0,
     margin: 0,
-    width:"100%"
+    width: "100%",
   },
   menu: {
-    flexDirection: 'column',
-    alignItems: 'center',    
-    width:"100%"
-    
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
   },
   item: {
     padding: 10,
     margin: 5,
     backgroundColor: "#014786",
-    color:"#FFFFFF",
+    color: "#FFFFFF",
     fontSize: fontPixel(30),
-    alignItems: 'center',
+    alignItems: "center",
     justifyContent: "center",
-    borderWidth: 1, 
-    borderStyle: "solid", 
+    borderWidth: 1,
+    borderStyle: "solid",
     borderColor: "#000000",
-    width:"100%"
+    width: "100%",
   },
 });
---
+++ config/index.js
@@ -0,0 +1,3 @@
+export default {
+  COLOR1: "red",
+};
